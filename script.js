3c479275cbe5ce2b59a53ca6e95cdeb013ae8531
meryzennn <85497760+meryzennn@users.noreply.github.com>
2024-08-04 14:29:56 +0700

    add note on js

--- script.js
+++ script.js
@@ -13,14 +13,14 @@ document.querySelector('#push').onclick = function(){
                 </button>
             </div>
         `;
-
+        // fungsi-delete
         var current_tasks = document.querySelectorAll(".delete");
         for(var i=0; i<current_tasks.length; i++){
             current_tasks[i].onclick = function(){
                 this.parentNode.remove();
             }
         }
-
+        // centang
         var tasks = document.querySelectorAll(".task");
         for(var i=0; i<tasks.length; i++){
             tasks[i].onclick = function(){
